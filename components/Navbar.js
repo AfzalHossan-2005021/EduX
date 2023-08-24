31f87cf2197991de018c7fb8f2ff6af1aedc8ac2
Afzal_Hossan <afzalhossan2005021@gmail.com>
2023-08-24 23:03:26 +0600

    Search result box complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import Image from 'next/image'
 import Logo from '../public/T_logo.png'
 import { FaSearch } from 'react-icons/fa'
-import { Dropdown } from "flowbite-react";
+import { AiOutlineCaretUp, AiOutlineCaretDown } from 'react-icons/ai'
 
 const Navbar = () => {
   const [results, setResults] = useState([]);
   return (
-    <nav className="top-0 h-16 left-0 right-0 bg-slate-200 shadow-2xl">
-      <div className="flex h-full p-3 md:space-x-5 ">
-        <div className='pb-4'>
-        <a href="/">
-          <Image src={Logo} alt="Logor" height='50'/>
-        </a>
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-gradient-to-r from-slate-200 to-slate-400 shadow-2xl">
+      <div className="flex h-full p-3 md:space-x-5 justify-between">
+        <div className='absolute'>
+          <a href="/">
+            <Image src={Logo} alt="Logor" height='50' />
+          </a>
         </div>
         <ExploreDropDown />
-        <SearchBar setResults={setResults} />
+        <div className='w-2/5 md:space-y-12'>
+          <SearchBar setResults={setResults} />
+          <SearchResultsList results={results} />
+        </div>
         <LogIn_SignUp />
       </div>
     </nav>
@@ -23,63 +26,77 @@ const Navbar = () => {
 };
 
 function ExploreDropDown() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Dropdown label="Explore" height = '10'>
-      <div className='flex'>
-        <div className='column'>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Online Stores</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Segmentation</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Marketing CRM</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-        </div>
-        <div className='column'>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Online Stores</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Segmentation</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-          <Dropdown.Item>
-            <a href="#" className="block p-3 rounded-lg">
-              <div className="font-semibold">Marketing CRM</div>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                Connect with third-party tools that you are already using.
-              </span>
-            </a>
-          </Dropdown.Item>
-        </div>
-      </div>
-    </Dropdown>
+    <div className='absolute left-48 md:space-y-12'>
+      <button className='absolute bg-blue-600 hover:bg-blue-700 flex items-center w-32 h-10 shadow-xlr justify-between p-2 font-bold text-lg rounded-l-lg tracking-wider border-transparent border-4 duration-300 active:text-white active:border-white'
+        onClick={() => setIsOpen((prev) => !prev)}>
+        Explore
+        {
+          !isOpen ? (
+            <AiOutlineCaretDown />
+          ) : (
+            <AiOutlineCaretUp />
+          )
+        }
+      </button>
+      {
+        isOpen && (
+          // <div className='bg-blue-400 absolute top-20 flex flex-col items-start rounded-lg p-2 w-full'>
+          //   {
+          //     list.map((item, i) => {
+          //       <div className='flex w-full justify-between hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4'>
+          //         <h3>{item.city}</h3>
+          //         <h3>{item.emotion}</h3>
+          //       </div>
+          //     })
+          //   }
+          // </div>
+          <div className='relative bg-white flex shadow-xl'>
+            <div className='column'>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Online Stores</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Segmentation</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Marketing CRM</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+            </div>
+            <div className='column'>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Online Stores</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Segmentation</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+              <a href="#" className="hover:bg-blue-200 block p-3">
+                <div className="font-semibold">Marketing CRM</div>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Connect with third-party tools that you are already using.
+                </span>
+              </a>
+            </div>
+          </div>
+        )
+      }
+    </div>
   );
 }
 
@@ -103,29 +120,44 @@ function SearchBar({ setResults }) {
   };
 
   return (
-    <div className='bg-white w-2/5 h-10 rounded-lg shadow-lg px-0 flex items-center'>
-      <div className='pl-2'>
-        <FaSearch color='royalblue' size='25px' />
-      </div>
-      <div className='w-full'>
-        <input className='bg-transparent border-transparent h-full w-full text-xl ml-1.5 outline-none focus:border-blue-500'
-          placeholder='Type to search...' value={input} onChange={(e) => handleChange(e.target.value)} />
-      </div>
+    <div className='absolute left-[340px] bg-white w-[498px] h-10 rounded-r-lg shadow-lg px-0 flex items-center'>
+      <input className='bg-transparent h-full w-[440px] text-xl ml-1.5 outline-none'
+        placeholder='Type to search...' value={input} onChange={(e) => handleChange(e.target.value)} />
+      <button className='absolute left-[450px] bg-blue-600 hover:bg-blue-700 border-none w-12 h-10 rounded-r-lg p-3'>
+        <FaSearch color='white' size='20px' />
+      </button>
+    </div>
+  );
+}
+
+function SearchResultsList({ results }) {
+  return (
+    <div className='relative left-[310px] w-full bg-white flex-col shadow-md max-h-80 overflow-auto'>
+      {
+        results.map((result, id) => {
+          return (
+            <div key={id} className='py-5 px-2.5 hover:bg-zinc-600 text-sky-600'
+              onClick={(e) => alert(`you clicked on {result}`)}>
+              {result.name}
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
 
 function LogIn_SignUp() {
   return (
-    <div className='flex md:space-x-5'>
+    <div className='absolute left-[840px] flex md:space-x-5'>
       <div className='md:pl-[448px] sm: pl-16'>
         <a href="/login">
-          <button className="bg-blue-700 hover:bg-blue-800 font-semibold text-white items-center w-20 h-10 rounded-lg shadow-xl">Log In</button>
+          <button className="bg-blue-600 hover:bg-blue-700 font-semibold text-white items-center w-20 h-10 rounded-lg shadow-xl">Log In</button>
         </a>
       </div>
       <div>
         <a href="/signup">
-          <button className="bg-blue-700 hover:bg-blue-800 font-semibold text-white items-center w-20 h-10 rounded-lg shadow-xl">Sign Up</button>
+          <button className="bg-blue-600 hover:bg-blue-700 font-semibold text-white items-center w-20 h-10 rounded-lg shadow-xl">Sign Up</button>
         </a>
       </div>
     </div>
