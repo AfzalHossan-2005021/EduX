cc1d5e58c4f20af2b9e6fd1bc8d79d17cc310dda
Amin-2005022 <amin.ekramul012@gmail.com>
2023-09-01 16:39:32 +0600

    merged

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/navigation';
 
-
 export default function login({ isLoggedIn, setIsLoggedIn }) {
 
   const router = useRouter();
@@ -19,9 +18,6 @@ export default function login({ isLoggedIn, setIsLoggedIn }) {
       setError("All fields are necessary");
       setIsErrorOccured(true);
     }
-
-
-
     else {
       event.preventDefault();
       const data = { email, password };
@@ -71,8 +67,8 @@ export default function login({ isLoggedIn, setIsLoggedIn }) {
               <h2 className='text-xl font-semibold text-lime-500 text-center'>Log in</h2>
               {
                 isErrorOccured && (
-                  <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                    <span className="block sm:inline">{error}</span>
+                  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <span class="block sm:inline">{error}</span>
                   </div>
                 )
               }
