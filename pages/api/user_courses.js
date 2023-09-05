57c6c6da20c2473af8fe7c0323d3d429e6d1aaaf
Afzal_Hossan <afzalhossan2005021@gmail.com>
2023-09-06 03:56:26 +0600

    user_courses api created

---
+++ pages/api/user_courses.js
@@ -0,0 +1,27 @@
+const oracledb = require('oracledb');
+import pool from "../../middleware/connectdb"
+
+
+export default async function handler(req, res) {
+    if (req.method == 'POST') {
+        const connection = await pool.acquire();
+        const { u_id } = req.body;
+        try {
+            const result = await connection.execute(
+                `BEGIN
+                    USER_COURSES(:user_id);
+                  END;`,
+                { user_id: u_id },
+                { outFormat: oracledb.OUT_FORMAT_OBJECT }
+            );
+            res.status(200).json(result.implicitResults);
+        } catch (error) {
+            res.status(500).json({ message: 'An error occurred.' });
+        } finally {
+            pool.release(connection);
+        }
+    }
+    else {
+        res.status(400).json({ message: 'This method is not allowed.' })
+    }
+}
