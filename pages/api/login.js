8a589d057c213774a2bb4c1b99ed9a57a82ff47b
Amin-2005022 <amin.ekramul012@gmail.com>
2023-09-07 20:46:34 +0600

    abc

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,30 +2,46 @@ const oracledb = require('oracledb');
 import pool from '../../middleware/connectdb'
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
-        const connection = await pool.acquire();
-        const { email, password } = req.body;
-        try {
-            const result = await connection.execute(
-                `BEGIN
-                    :message := CHECK_USER(:email, :password);
-                END;`,
-                {
-                    email: email,
-                    password: password,
-                    message: { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 100 }
-                },
-                { outFormat: oracledb.OUT_FORMAT_OBJECT }
-            );
-            res.status(200).json(result.outBinds);
-        } catch (error) {
-            res.status(500).json({ message: 'An error occurred.' });
-        } finally {
-            pool.release(connection);
-        }
-    }
-    else {
-        res.status(400).json({ message: 'This method is not allowed.' })
-    }
-}
+  if (req.method == 'POST') {
+    const connection = await pool.acquire();
+    const { email, password } = req.body;
+    try {
+      console.log("in api login ")
+      const result = await connection.execute(
+        `BEGIN
+            CHECK_USER(:email, :password, :user_id, :user_name);
+          END;`,
+        {
+          email: email,
+          password: password,
+          user_id: { dir: oracledb.BIND_OUT, type: oracledb.NUMBER },
+          user_name: { dir: oracledb.BIND_OUT, type: oracledb.STRING },
+        },
+        { outFormat: oracledb.OUT_FORMAT_OBJECT },
+      );
+      console.log("in api login 2");
+      let message = '';
+      let success = false;
+      const u_id = result.outBinds.user_id;
+      const u_name = result.outBinds.user_name;
 
+      if (u_id == -1) {
+        message = 'Invalid password';
+        success = false;
+      } else if (u_id == -2) {
+        message = 'User is not registered';
+        success = false;
+      } else {
+        message = 'Valid user';
+        success = true;
+      }
+      res.status(200).json({ success, message, u_id , u_name});
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred.' });
+    } finally {
+      pool.release(connection);
+    }
+  } else {
+    res.status(400).json({ message: 'This method is not allowed.' });
+  }
+}
--- pages/login.js
+++ pages/login.js
@@ -64,8 +64,8 @@ export default function login() {
               <h2 className='text-xl font-semibold text-lime-500 text-center'>Log in</h2>
               {
                 isErrorOccured && (
-                  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                    <span class="block sm:inline">{error}</span>
+                  <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <span className="block sm:inline">{error}</span>
                   </div>
                 )
               }
