4f69141b7c34e6b33e21463833c4b0c8e61ed7d1
Afzal_Hossan <afzalhossan2005021@gmail.com>
2023-11-17 11:49:50 +0600

    Quiz Question add.

--- pages/api/update_mark.js
+++ pages/api/update_mark.js
@@ -1,31 +1,29 @@
-const oracledb = require('oracledb');
-import pool from "../../middleware/connectdb"
+const oracledb = require("oracledb");
+import pool from "../../middleware/connectdb";
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
-        const connection = await pool.acquire();
-        const { s_id, e_id, score } = req.body;
-        console.log(s_id, e_id, score);
-        try {
-            const result = await connection.execute(
-                `BEGIN
-                    SECURE_EXAM(:s_id, :e_id, :marks);
-                END;`,
-                {
-                    s_id: s_id,
-                    e_id: e_id,
-                    marks: score
-                },
-                { outFormat: oracledb.OUT_FORMAT_OBJECT }
-            );
-            res.status(200).json(result.rows);
-        } catch (error) {
-            res.status(500).json({ message: 'An error occurred.' });
-        } finally {
-            pool.release(connection);
-        }
+  if (req.method == "POST") {
+    const connection = await pool.acquire();
+    const { s_id, e_id, score } = req.body;
+    try {
+      const result = await connection.execute(
+        `BEGIN
+            SECURE_EXAM(:s_id, :e_id, :marks);
+        END;`,
+        {
+          s_id: s_id,
+          e_id: e_id,
+          marks: score,
+        },
+        { outFormat: oracledb.OUT_FORMAT_OBJECT }
+      );
+      res.status(200).json(result.rows);
+    } catch (error) {
+      res.status(500).json({ message: "An error occurred." });
+    } finally {
+      pool.release(connection);
     }
-    else {
-        res.status(400).json({ message: 'This method is not allowed.' })
-    }
+  } else {
+    res.status(400).json({ message: "This method is not allowed." });
+  }
 }
--- pages/user/courses/topic/exam/[e_id].js
+++ pages/user/courses/topic/exam/[e_id].js
@@ -1,29 +1,34 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import secureLocalStorage from 'react-secure-storage';
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import QuizQuestion from "@/components/QuizQuestion";
+import secureLocalStorage from "react-secure-storage";
 
 export const getServerSideProps = async (context) => {
-  const { params } = context
-  const { e_id } = params
-  return { props: { e_id } }
-}
+  const { params } = context;
+  const { e_id } = params;
+  return { props: { e_id } };
+};
 
 export default function userCourseInfo({ e_id }) {
   const router = useRouter();
-  const s_id = secureLocalStorage.getItem('u_id');
+  const s_id = secureLocalStorage.getItem("u_id");
   const [questions, setQuestions] = useState([]);
-  const [selectedOptions, setSelectedOptions] = useState(new Array(5).fill(null));
+  const [selectedOptions, setSelectedOptions] = useState(
+    new Array(5).fill(null)
+  );
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/exam_questions', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ e_id })
-    }).then((res) => {
-      return res.json();
-    }).then((json_res) => {
-      setQuestions(json_res);
-    });
+    fetch("http://localhost:3000/api/exam_questions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ e_id }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json_res) => {
+        setQuestions(json_res);
+      });
   }, []);
 
   const handleOptionSelect = (questionIndex, option) => {
@@ -34,17 +39,19 @@ export default function userCourseInfo({ e_id }) {
 
   const handleSubmit = () => {
     let newScore = 0;
+    let answers = [];
     for (let i = 0; i < questions.length; i++) {
       if (selectedOptions[i] == questions[i].right_ans) {
         newScore += questions[i].marks;
       }
+      answers.push({ q_id: questions[i].q_id, ans: selectedOptions[i] });
     }
-    fetch('http://localhost:3000/api/update_mark', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ s_id, e_id, score: newScore })
+    fetch("http://localhost:3000/api/update_mark", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ s_id, e_id, score: newScore }),
     }).then(() => {
-      router.replace(`/user/courses/topic/exam/result/${e_id}`)
+      router.replace(`/user/courses/topic/exam/result/${e_id}`);
     });
   };
 
@@ -52,29 +59,13 @@ export default function userCourseInfo({ e_id }) {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-semibold mb-4">MCQ Quiz</h1>
       {questions.map((question, index) => (
-        <div key={index} className="mb-4">
-          <h2 className="text-lg font-semibold mb-2">Question {question.serial}</h2>
-          <p className="mb-2">{question.q_description}</p>
-          <ul>
-            {['opt1', 'opt2', 'opt3', 'opt4'].map((optionKey, optionIndex) => (
-              <li
-                key={optionIndex}
-                className={`mb-2 flex items-center cursor-pointer`}
-              >
-                <input
-                  type="radio"
-                  id={`q${index}_option${optionIndex}`}
-                  name={`q${index}`}
-                  value={question[optionKey]}
-                  checked={selectedOptions[index] == optionIndex+1}
-                  onChange={() => handleOptionSelect(index, optionIndex+1)}
-                  className="mr-2"
-                />
-                <label htmlFor={`q${index}_option${optionIndex}`}>{question[optionKey]}</label>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <QuizQuestion
+        key={index}
+        question={question}
+        index={index}
+        selectedOption={selectedOptions[index]}
+        onOptionSelect={handleOptionSelect}
+      />
       ))}
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
